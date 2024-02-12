cf380209c7ba1b86753137fa73d7d19aa864d4cb
montasir12345 <155537115+montasir12345@users.noreply.github.com>
2024-02-12 12:15:52 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let toggle = document.querySelector(".toggle")
+let menu = document.querySelector(".menu")
+
+toggle.onclick = function(){
+    menu.classList.toggle("active")
+}
+
+
+
+
+
+document.querySelector('.tw').addEventListener('click', function(){
+    alert("No twitter")
+}) 
+document.querySelector('.dr').addEventListener('click', function(){
+    alert("No dribbble")
+}) 
+document.querySelector('.lin').addEventListener('click', function(){
+    alert("No linkedin")
+}) 
+document.querySelector('.ins').addEventListener('click', function(){
+    alert("No instagram")
+}) 
+document.querySelector('.pin').addEventListener('click', function(){
+    alert("No pinterest")
+}) 
+document.querySelector('.you').addEventListener('click', function(){
+    alert("No youtube")
+})
+
+
+
+
